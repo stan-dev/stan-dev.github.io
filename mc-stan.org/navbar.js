d9a0a22a9411333019d36b59ab80ef1d1e5b5c7f
Bob Carpenter <carp@alias-i.com>
2013-10-28 18:32:15 -0400

    intermediate web page commit

---
+++ mc-stan.org/navbar.js
@@ -0,0 +1,21 @@
+document.write('\
+<div style="margin: 0em 1em 0em 0.25em">\
+  <a href="rstan.html" style="font-size: 85%">RStan</a>\
+  &nbsp; | &nbsp;\
+  <a href="pystan.html" style="font-size: 85%">PyStan</a>\
+  &nbsp; | &nbsp;\
+  <a href="cmdstan.html" style="font-size: 85%">CmdStan</a>\
+  &nbsp; || &nbsp;\
+  <a href="manual.html" style="font-size: 85%">Manual</a>\
+  &nbsp; | &nbsp;\
+  <a href="examples.html" style="font-size: 85%">Examples</a>\
+  &nbsp; || &nbsp;\
+  <a href="issues.html" style="font-size: 85%">Issues</a>\
+  &nbsp; | &nbsp;\
+  <a href="groups.html" style="font-size: 85%">Groups</a>\
+  &nbsp; | &nbsp;\
+  <a href="citations.html" style="font-size: 85%">Cites</a>\
+  &nbsp; | &nbsp;\
+  <a href="contributions.html" style="font-size: 85%">Contribute</a>\
+</div>\
+');
---
+++ mc-stan.org/navfoot.js
@@ -0,0 +1,6 @@
+document.write('\
+<div style="margin:0 0 0 2.25em">\
+<span style="font-size:85%">Back to: &nbsp;</span>\
+<a href="index.html" style="font-size:85%">Stan Home Page</a>\
+</div>\
+');
