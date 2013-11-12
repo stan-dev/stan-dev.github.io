307fdafc59f1d12c2ad149d41a1c70839978f0c1
Bob Carpenter <carp@alias-i.com>
2013-11-11 19:22:04 -0500

    copy-edited all pages, adjusting styles a bit

---
+++ mc-stan.org/nav.js
@@ -0,0 +1,25 @@
+document.write('\
+<div id="nav-container">\
+  <ul>\
+    <li><a href="index.html">Home</a></li>\
+    <div class="nav-sep"></div>\
+    <li><a href="rstan.html">RStan</a></li>\
+    <li><a href="pystan.html">PyStan</a></li>\
+    <li><a href="cmdstan.html">CmdStan</a></li>\
+    <div class="nav-sep"></div>\
+    <li><a href="manual.html">Manual</a></li>\
+    <li><a href="examples.html">Examples</a></li>\
+    <div class="nav-sep"></div>\
+    <li><a href="groups.html">Groups</a></li>\
+    <li><a href="issues.html">Issues</a></li>\
+    <div class="nav-sep"></div>\
+    <li><a href="citations.html">Citations</a></li>\
+    <li><a href="contribute.html">Contribute</a></li>\
+    <div class="nav-sep"></div>\
+    <li><a href="team.html">Team</a></li>\
+    <li><a href="source-repos.html">Source Repos</a></li>\
+  </ul>\
+</div>\
+');
+
+  
